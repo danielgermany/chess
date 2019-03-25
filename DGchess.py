56d956f78c6f2047404ef47ba9cb61bc97d989e8
danielgermany <45012910+danielgermany@users.noreply.github.com>
2019-03-25 12:18:07 -0400

    Adding code

---
+++ DGchess.py
@@ -0,0 +1,75 @@
+from graphics import *
+
+def drawBoard(sZ,window):
+    chessBackground = Rectangle(Point(sZ,sZ),Point(sZ * 9,sZ * 9))
+    chessBackground.setFill(color_rgb(245,245,220));chessBackground.draw(chessWin)
+    for y in range(8):
+        for x in range(8):
+            chessRect = Rectangle(Point(sZ * (x + 1),sZ * (y + 1)),
+                                    Point(sZ * (x + 2),sZ * (y + 2)))
+            if ((y/2) % 1 == 0):
+                if ((x/2) % 1 == 0) == False: chessRect.setFill(color_rgb(50,205,50))
+            else:
+                if ((x/2) % 1 == 0): chessRect.setFill(color_rgb(50,205,50))
+            chessRect.draw(window)
+
+def InitBoardState():
+    boardState = [[" " for x in range(8)]
+                   for y in range(8)]
+
+    for y in range(8):
+        for x in range(8):
+            if y == 0:
+                if x == 0:
+                    boardState[x][y] = "WR1"
+                elif x == 1:
+                    boardState[x][y] = "WN1"
+                elif x == 2:
+                    boardState[x][y] = "WB1"
+                elif x == 3:
+                    boardState[x][y] = "WQ"
+                elif x == 4:
+                    boardState[x][y] = "WK"
+                elif x == 5:
+                    boardState[x][y] = "WB2"
+                elif x == 6:
+                    boardState[x][y] = "WN2"
+                elif x == 7:
+                    boardState[x][y] = "WR2"
+            elif y == 1:
+                boardState[x][y] = "WP" + str(x + 1)
+            elif y == 6:
+                if x == 0:
+                    boardState[x][y] = "BR1"
+                elif x == 1:
+                    boardState[x][y] = "BN1"
+                elif x == 2:
+                    boardState[x][y] = "BB1"
+                elif x == 3:
+                    boardState[x][y] = "BQ"
+                elif x == 4:
+                    boardState[x][y] = "BK"
+                elif x == 5:
+                    boardState[x][y] = "BB2"
+                elif x == 6:
+                    boardState[x][y] = "BN2"
+                elif x == 7:
+                    boardState[x][y] = "BR2"
+            elif y == 7:
+                boardState[x][y] = "BP" + str(x + 1)
+            print(boardState[x][y])
+
+def InitBoardGrid():
+    boardGrid = [[" " for x in range(8)]
+                   for y in range(8)]
+
+print("Size of window?(500 recommended)");windowSize = int(input())
+squareSize = (windowSize/10)
+chessWin = GraphWin("Chess", windowSize,windowSize);chessWin.setCoords(0,0, windowSize,windowSize)
+q = 0
+drawBoard(squareSize,chessWin)
+InitBoardState()
+
+while True:
+    q = q + 1
+    print("Hello1")
